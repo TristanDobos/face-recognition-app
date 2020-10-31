cf795ebd3b482c11c0224e2edd3f99d7d13292ba
TristanDobos <tristandobos37@gmail.com>
2020-10-31 20:00:16 +0100

    Updates

---
+++ precache-manifest.0ff0a3e9202465bdff68fbb59de467f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1219c078f97152f9000aa626245fc129",
+    "url": "/face-recognition-app/index.html"
+  },
+  {
+    "revision": "633eb42d845caac4708c",
+    "url": "/face-recognition-app/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b4ff1d6bc013d06bd530",
+    "url": "/face-recognition-app/static/css/main.e279e08b.chunk.css"
+  },
+  {
+    "revision": "633eb42d845caac4708c",
+    "url": "/face-recognition-app/static/js/2.2c261e76.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/face-recognition-app/static/js/2.2c261e76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4ff1d6bc013d06bd530",
+    "url": "/face-recognition-app/static/js/main.30580991.chunk.js"
+  },
+  {
+    "revision": "1dddd6d71149c78ac8b0",
+    "url": "/face-recognition-app/static/js/runtime-main.238bf622.js"
+  }
+]);
