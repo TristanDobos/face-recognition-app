5e4c82ce03b872c24c2337348f4545299e0aa8c4
TristanDobos <tristandobos37@gmail.com>
2021-02-22 11:36:11 +0100

    Updates

---
+++ precache-manifest.a5f624598736340dc8697a747cf77711.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57ddc878f2594ba79fe40bc7b68bc576",
+    "url": "/face-recognition-app/index.html"
+  },
+  {
+    "revision": "dc4434e91b9e5a5fe3ba",
+    "url": "/face-recognition-app/static/css/2.038644eb.chunk.css"
+  },
+  {
+    "revision": "c07be26351f2dc9f4d32",
+    "url": "/face-recognition-app/static/css/main.f9a79f76.chunk.css"
+  },
+  {
+    "revision": "dc4434e91b9e5a5fe3ba",
+    "url": "/face-recognition-app/static/js/2.8ae48466.chunk.js"
+  },
+  {
+    "revision": "28d36cae8db7790f6fc02920470bed26",
+    "url": "/face-recognition-app/static/js/2.8ae48466.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c07be26351f2dc9f4d32",
+    "url": "/face-recognition-app/static/js/main.926a656d.chunk.js"
+  },
+  {
+    "revision": "1dddd6d71149c78ac8b0",
+    "url": "/face-recognition-app/static/js/runtime-main.238bf622.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/face-recognition-app/precache-manifest.f3aad8710e33b4ea5f2bc201532b8111.js"
+  "/face-recognition-app/precache-manifest.a5f624598736340dc8697a747cf77711.js"
 );
 
 self.addEventListener('message', (event) => {
